44c552863f4f776f7dc9aa425706fdeaa6d85dae
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2024-05-07 19:56:12 +0300

    Register Patient Done (Front & Back)

--- PlasticSurgeryClinic/Clinic/urls.py
+++ PlasticSurgeryClinic/Clinic/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("register", views.register_patient, name='register'),
     path("admin", views.admin, name="admin"),
     path("doctor", views.doctor_view, name='doctor'),
     path("nurse", views.nurse_view, name='nurse'),
+   
 ]
