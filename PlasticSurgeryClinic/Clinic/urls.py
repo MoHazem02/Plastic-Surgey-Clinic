350e2ad234db9ef9d4ced4cfb72b2f422057026a
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2024-05-20 18:33:33 +0300

    Removed editProfile

--- PlasticSurgeryClinic/Clinic/urls.py
+++ PlasticSurgeryClinic/Clinic/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path("doctor", views.doctor_view, name='doctor'),
     path("nurse", views.nurse_view, name='nurse'),
     path("patient", views.patient_view, name='patient'),   
-    path("edit_profile", views.edit_profile, name='edit_profile'),
     path("doctors", views.doctors, name='view doctors'),
     path("nurses", views.nurses, name='view nurses'),
     path("Add_Staff", views.add_staff, name='add_staff'),
